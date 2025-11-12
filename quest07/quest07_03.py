1d62391a079875db9db79e54610c61f72a97cc07
Kai Zhi Chiang <kchiang@u91z01s06.42kl.edu.my>
2025-11-12 20:11:28 +0800

    troubleshooting quest07_03

--- quest07/quest07_03.py
+++ quest07/quest07_03.py
@@ -13,45 +13,61 @@ def generate_list(name, rules, list):
     if (tail in rules):
         for i in range(len(rules[tail])):
             tmp = name + rules[tail][i]
-            print(tmp)
             if (7 <= len(tmp) <= 11):
                 list.append(tmp)
             generate_list(tmp, rules, list)
 
-
 def main():
-    data = "Khara,Xaryt,Noxer,Kharax"
+    data = "Ny,Nyl,Nyth,Nyss,Nyrix,Pald,Cael,Fen,Gorath,Ryth,Zyrix,Gar,Grim,Xaral,Elar,Skar,Tharn,Rylar,Bryl,Nex"
     names = data.split(',')
     rules = {
-        'r': "veagy",
-        'a': "evxrg",
-        'e': "rxvt",
-        'h': "aev",
-        'g': "ry",
-        'y': "pt",
-        'i': "vr",
-        'K': "h",
-        'v': "e",
-        'B': "r",
-        't': "h",
-        'N': "e",
-        'p': "h",
-        'H': "e",
-        'l': "t",
-        'z': "e",
+        's': "asjfcxrzt",
+        'E': "l",
         'X': "a",
-        'n': "v",
-        'x': "z",
-        'T': "i"
+        'N': "ey",
+        'r': "aiejfcxrzstvn",
+        'F': "e",
+        'i': "soxm",
+        'l': "yojfcxrzstdva",
+        'a': "rlvt",
+        'e': "xtlv",
+        'Z': "y",
+        'G': "oar",
+        't': "hy",
+        'k': "v",
+        'j': "o",
+        'C': "a",
+        'T': "h",
+        'o': "rnv",
+        'R': "y",
+        'y': "vxrl",
+        'n': "jfcxrzst",
+        'x': "sejfcxrzt",
+        'B': "r",
+        'd': "jfcxrzst",
+        'S': "k",
+        'f': "ae",
+        'c': "a",
+        'h': "jfcxrzstv",
+        'z': "ei",
+        'P': "a",
+        'm': "jfcxrzst"
     }
     total = 0
     for name in names:
         if (name_fit_rules(name, rules) == False):
             names.remove(name)
-    print(names)
+    for name in names:
+        for i in names:
+            if i.startswith(name) == True:
+                names.remove(name)
+                break
     list = []
-    for name in range(len(names)):
+    for name in names:
         generate_list(name, rules, list)
+    list = sorted(list)
+    for name in list:
+        print(name)
     print(len(list))
 
 if __name__ == "__main__":
