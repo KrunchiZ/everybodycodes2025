a69f96a7052f450e94c0327aeb701da68b67ca1f
Kai Zhi Chiang <kchiang@u91z01s06.42kl.edu.my>
2025-11-14 19:02:18 +0800

    quest9 wip

--- quest09/quest09_01.py
+++ quest09/quest09_01.py
@@ -1,6 +1,10 @@
 def main():
     data = {
+        1: "CAAGCGCTAAGTTCGCTGGATGTGTGCCCGCG",
+        2: "CTTGAATTGGGCCGTTTACCTGGTTTAACCAT",
+        3: "CTAGCGCTGAGCTGGCTGCCTGGTTGACCGCG"
     }
+    index = {None} * len(data)
     return
 
 if __name__ == "__main__":
