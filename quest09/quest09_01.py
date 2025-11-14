5d6c9d85ee3ae27b4fc5696ad8344bd58ee6baff
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-11-15 02:07:15 +0800

    finished quest09_01

--- quest09/quest09_01.py
+++ quest09/quest09_01.py
@@ -1,10 +1,24 @@
+def count_matches(data, i):
+    total = 1
+    for j in range(1, 4):
+        count = 0
+        if (j != i):
+            for a in range(len(data[i])):
+                if (data[i][a] == data[j][a]):
+                    count += 1
+            total *= count
+    return (total)
+
 def main():
     data = {
-        1: "CAAGCGCTAAGTTCGCTGGATGTGTGCCCGCG",
-        2: "CTTGAATTGGGCCGTTTACCTGGTTTAACCAT",
-        3: "CTAGCGCTGAGCTGGCTGCCTGGTTGACCGCG"
+    1: "CGGCAGTCTCGAAGGGTACTGTGTGCACGCGTCATTTTGATGGGACCTCCTAAAAACCGCGTGATCCACCCTTTCATTCTATGCGCCGGACGCACCCCGGGTTAAGTGGCATCCGCGGTAACTGATAA",
+    2: "AATGCTAGTCTATAAGGGCCTTGGTCTTTCGGAAATGGCCCTTATAGATGAGGTACGCTTGGGCCAGCTGTTCGGGTAGAACTTCGGACGGTTCGGCTGATGTGTAGAAACGCTGAAGACTACTGGGG",
+    3: "AATGAGTCTCGAAAGGTGCTGTGGGCTCGCGGCATTTGCCTTTATACTCGTAATAACCTTGTGATCGACGTTTTGATTGTATGCGGCGGGGTTAGCCTGAGGTAAGGAACCTCCGAGGTAAACGAGGG"
     }
-    index = {None} * len(data)
+    index = {}
+    for i in range(1, 4):
+        index[i] = count_matches(data, i)
+    print(index[max(index)])
     return
 
 if __name__ == "__main__":
