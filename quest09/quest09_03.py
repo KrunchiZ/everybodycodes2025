4a018e9a32525adb55aa98efa47708c20e543f0a
Kai Zhi Chiang <kchiang@u91z01s06.42kl.edu.my>
2025-11-15 20:48:10 +0800

    screw this shit

--- quest09/quest09_03.py
+++ quest09/quest09_03.py
@@ -16,33 +16,6 @@ def find_parents(data, i):
         x += 1
     return [0, 0]
 
-def find_ancestors(parents, i):
-    list = parents[i]
-    if list == [0, 0]:
-        return []
-    list.extend(find_ancestors(parents, parents[i][0]))
-    list.extend(find_ancestors(parents, parents[i][1]))
-    return list
-
-def find_descendants(parents, x):
-    list = []
-    a = 1
-    while a <= len(parents):
-        if x == parents[a][0] or x == parents[a][1]:
-            list.append(a)
-            list.extend(find_descendants(parents, a))
-        a += 1
-    return list
-
-def find_siblings(parents, i):
-    list = []
-    if parents[i] == [0, 0]:
-        return list
-    for a in range(1, len(parents) + 1):
-        if a!= i and parents[a] == parents[i]:
-            list.append(a)
-    return list
-
 def main():
     data = {
         1: "GCAGGCGAGTATGATACCCGGCTAGCCACCCC",
@@ -55,17 +28,9 @@ def main():
         8: "GGCGTAAAGTATGGATGCTGGCTAGGCACCCG",
     }
     parents = {}
-    scales = {}
     for i in range(1, len(data) + 1):
         parents[i] = find_parents(data, i)
-    for i in range(1, len(parents) + 1):
-        scales[i] = find_ancestors(parents, i)
-    for i in range(1, len(parents) + 1):
-        scales[i].extend(find_descendants(parents, i))
     print(parents)
-    for i in range(1, len(parents) + 1):
-        scales[i].extend(find_siblings(parents, i))
-    print(scales)
     return
 
 if __name__ == "__main__":
